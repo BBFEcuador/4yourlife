2010a44c96fc4cf416e4cfd1a9ecb0297c461083
Jair Guanotoa <jairguanotoa@hotmail.com>
2025-01-15 10:22:22 -0500

    build: :construction: Added persitence annotations and sql migration
    Persitence added in Role entity to map migration to database, added sql file with role table and join table between role and permission

--- src/main/java/com/foryourlife/account/Role.java
+++ src/main/java/com/foryourlife/account/Role.java
@@ -1,20 +1,32 @@
 package com.foryourlife.account;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import com.foryourlife.account.permission.domain.Permissions;
+
+import java.util.Set;
 
 @Entity
+@Table(name = "roles")
 public class Role {
 
+    @Id
     private String id;
     private String roleName;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "account_roles_permissions",
+            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id", referencedColumnName = "id"))
+    private Set<Permissions> permissions;
 
     protected Role() {
 
     }
 
-    private Role(String id, String roleName) {
+    private Role(String id, String roleName, Set<Permissions> permissions) {
         this.id = id;
         this.roleName = roleName;
+        this.permissions = permissions;
     }
 
     public String getId() {
@@ -25,8 +37,14 @@ public class Role {
         return roleName;
     }
 
-    public Role create(String id, String roleName) {
-        var newRole = new Role(id, roleName);
+    public void addPermission(Permissions permission) {
+        if (!this.permissions.contains(permission)) {
+            this.permissions.add(permission);
+        }
+    }
+
+    public Role create(String id, String roleName, Set<Permissions> permissions) {
+        var newRole = new Role(id, roleName, permissions);
         return newRole;
     }
 }

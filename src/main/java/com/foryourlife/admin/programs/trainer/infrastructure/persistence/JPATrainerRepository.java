dcdc77d4ee1b1bae0ec0701989afdd9607e6e4dc
Xeexen <andres.o.mm@hotmail.com>
2025-03-10 11:02:28 -0500

    :memo: update trainer query

--- src/main/java/com/foryourlife/admin/programs/trainer/infrastructure/persistence/JPATrainerRepository.java
+++ src/main/java/com/foryourlife/admin/programs/trainer/infrastructure/persistence/JPATrainerRepository.java
@@ -4,6 +4,9 @@ import com.foryourlife.admin.programs.trainer.domain.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JPATrainerRepository extends JpaRepository<Trainer, String> {
+    List<Trainer> findByTeam_Empty ();
 }

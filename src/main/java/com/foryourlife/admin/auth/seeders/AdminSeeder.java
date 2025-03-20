2ef54f63a5cf3c910482f83375762f883779bfe8
PROPHET2350 <jorgeluis33ariosa@gmail.com>
2025-03-20 13:24:18 -0500

    modify mock

--- src/main/java/com/foryourlife/admin/auth/seeders/AdminSeeder.java
+++ src/main/java/com/foryourlife/admin/auth/seeders/AdminSeeder.java
@@ -181,7 +181,7 @@ public class AdminSeeder {
 
     private void makeParticipants() {
         var invitationToken = commandInvitationService.createInvitationByAdminWithQuantity(new InvitationRequest("3936ae5e-0cc1-4375-abc7-520d16999110", "600"));
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 450; i++) {
             var staffId = UUID.randomUUID().toString();
             var user = new User(
                     UUID.randomUUID().toString(),

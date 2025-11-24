b7337af07b7106641d0f7bf9fc09a646797b578a
Xeexen <andres.o.mm@hotmail.com>
2025-11-24 10:58:26 -0500

    :sparkles: Refactor: Remove unused JPAImplReportViewRepository dependency and related code in JPAReportViewRepository.

--- src/main/java/com/foryourlife/admin/crm/report/infrastructure/persistence/JPAImplReportViewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foryourlife.admin.crm.report.infrastructure.persistence;
-
-import com.foryourlife.admin.crm.report.domain.ReportView;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JPAImplReportViewRepository extends JpaRepository<ReportView, String> {
-}

159dbc266cdf405ca92f4af7e2a3563b6b0661e4
Xeexen <andres.o.mm@hotmail.com>
2026-01-19 14:22:48 -0500

    :sparkles: Feat: Add validation to ResetPasswordRequest; enforce non-null and non-blank constraints on newPassword field.

--- src/main/java/com/foryourlife/clients/account/participant/infrastructure/httpControllers/ResetPasswordRequest.java
+++ src/main/java/com/foryourlife/clients/account/participant/infrastructure/httpControllers/ResetPasswordRequest.java
@@ -1,6 +1,11 @@
 package com.foryourlife.clients.account.participant.infrastructure.httpControllers;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class ResetPasswordRequest {
+    @NotNull(message = "La nueva contraseña es requerida")
+    @NotBlank(message = "La nueva contraseña no puede estar vacía")
     public String newPassword;
 
     public String getNewPassword() {
